e33ed2fbb66389c2d90aefe700dee4bd9938acc6
ElDav1d <dvivoes@yahoo.es>
2024-07-08 14:59:25 +0200

    debounce input

--- src/features/filterList/FilterList.tsx
+++ src/features/filterList/FilterList.tsx
@@ -1,12 +1,25 @@
-import { LITERAL_FILTER_LIST_LABEL, LITERAL_FILTER_LIST_PLACEHOLDER } from '../../utils/constants';
+import { useState } from 'react';
+import {
+  DEBOUNCE_DELAY_FILTER_LIST,
+  LITERAL_FILTER_LIST_LABEL,
+  LITERAL_FILTER_LIST_PLACEHOLDER,
+} from '../../utils/constants';
 import { useFilterListActions } from './hooks';
+import { useDebounce } from '../../hooks';
 
 const Search = () => {
   const { setFilterList } = useFilterListActions();
+  const [searchString, setSearchString] = useState('');
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFilterList({ filterString: e.target.value });
+    setSearchString(e.target.value);
   };
+
+  useDebounce(searchString, DEBOUNCE_DELAY_FILTER_LIST, () => {
+    setFilterList({
+      filterString: searchString,
+    });
+  });
 
   return (
     <input

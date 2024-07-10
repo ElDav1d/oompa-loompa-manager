02e3e1997a90d86abd7cb7162b997735ebde986d
ElDav1d <dvivoes@yahoo.es>
2024-07-10 12:45:09 +0200

    persist list

--- src/middleware/persistState.ts
+++ src/middleware/persistState.ts
@@ -1,22 +1,21 @@
 import { Middleware } from '@reduxjs/toolkit';
 import { STORED_STATE } from '../utils/constants';
 import { setNewOompaDetail } from '../features/detail/oompaDetailSlice';
-import { setNewOompaItemStamp, updateNewOompaItemStamp } from '../features/list/oompaListSlice';
+import {
+  setNewOompaItemStamp,
+  setNewOompaList,
+  updateNewOompaItemStamp,
+} from '../features/list/oompaListSlice';
 
 const persistState: Middleware = (store) => (next) => (action: unknown) => {
   next(action);
 
-  // TODO - storage is retrieved from localStorage nad pushed again
-  // DRY if blocks: data cannot be manipulated in DB style
-  if (setNewOompaDetail.match(action)) {
-    localStorage.setItem(STORED_STATE, JSON.stringify(store.getState()));
-  }
-
-  if (setNewOompaItemStamp.match(action)) {
-    localStorage.setItem(STORED_STATE, JSON.stringify(store.getState()));
-  }
-
-  if (updateNewOompaItemStamp.match(action)) {
+  if (
+    setNewOompaDetail.match(action) ||
+    setNewOompaItemStamp.match(action) ||
+    updateNewOompaItemStamp.match(action) ||
+    setNewOompaList.match(action)
+  ) {
     localStorage.setItem(STORED_STATE, JSON.stringify(store.getState()));
   }
 };
